2d9f5a06b9ea48937a2caea70ec32042813caaf0
adityamishra4345 <adityamishra4345@gmail.com>
2026-07-07 14:26:06 +0530

    first commit

---
+++ Basics/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");//print will normally print but println add new line at end
+        System.out.print("pizza  ");
+        System.out.println("yay");
+        
+    }
+}
