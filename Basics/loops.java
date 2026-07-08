7447a2027d26a7937cf8d72378819006bcb9ee8f
adityamishra4345 <adityamishra4345@gmail.com>
2026-07-08 13:02:01 +0530

    LOOPS Done

--- Basics/loops.java
+++ Basics/loops.java
@@ -3,8 +3,31 @@ import java.util.*;
 
 public class loops {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //for loop
+        for(int i=1;i<11;i++){
+            System.out.println("2 * "+ i +"=" + (2*i));
+        }
         
-        
+
+        //while loop
+        int j=2;
+        while(j>0){
+            System.out.println("BONJOUR");
+            j--;
+        }
+
+
+
+        //do while loop here its xame as while but 1st do will execute in any condition like condition checking is done after first do
+        int a=2;
+        do{
+            System.out.println("Hii");
+            a--;
+        }
+        while(a>0);
+        sc.close();
     }
     
 }
