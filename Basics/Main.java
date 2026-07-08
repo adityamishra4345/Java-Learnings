f581035f44fccbc529fffb10837bd9b2c95dc6b3
adityamishra4345 <adityamishra4345@gmail.com>
2026-07-08 11:22:44 +0530

    input learned

--- Basics/Main.java
+++ Basics/Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");//print will normally print but println add new line at end
-        System.out.print("pizza  ");
-        System.out.println("yay");
-        
+          int age;
+       age=32;
+       double weight=82.5;
+
+       char a='!';
+
+       boolean is_available=true;
+
+       if(is_available){
+       System.out.println("user present");
+    }
+       else{
+        System.out.println("user absent");
+       }
+
+       String email="adityamishra@gmail.com";
+       System.out.print("your email is " + email);
+       
+       //print only prints but println prints then add a new line i.e println=>printline 
     }
 }
